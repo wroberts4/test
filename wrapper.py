2ce6c09a6ec9d778da5e24a37923322bea801ace
wroberts4 <wroberts4@wisc.edu>
2020-12-11 11:18:27 -0600

    import after setup

--- wrapper.py
+++ wrapper.py
@@ -1,12 +1,13 @@
 from git import Repo
 import pip
 from sys import argv
-from main import main
 
 
 def version(v):
     repo = Repo('.')
     repo.git.checkout(v)
     pip.main(['install', '.'])
+
+    from main import main
     main()
 
